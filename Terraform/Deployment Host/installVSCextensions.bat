1e143fbdeba91f8ffe12d24b2bb2b20d725780d9
gphill <gerald.hill@canada.ca>
2020-06-17 08:04:05 -0300

    Removed Deployment Host folder

--- Terraform/Deployment Host/installVSCextensions.bat
+++
@@ -1,7 +0,0 @@
-call code --install-extension eriklynd.json-tools
-call code --install-extension ms-vscode.azure-account
-call code --install-extension ms-vscode.azurecli
-call code --install-extension ms-vscode.powershell
-call code --install-extension msazurermtools.azurerm-vscode-tools
-call code --install-extension mauve.terraform
-call code --install-extension samcogan.arm-snippets
